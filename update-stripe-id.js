bafdc4769760ad5f44b8f7f4b772d2d5517ad100
Igor Raposo <igorraposo02@gmail.com>
2025-11-18 00:30:29 +0000

    atualização

--- update-stripe-id.js
+++ update-stripe-id.js
@@ -14,7 +14,7 @@ import { pool } from './src/db.js';
 async function run() {
   try {
     const slug = 'yoga-kula'; // <-- ALTERE AQUI PARA O SLUG CORRETO
-    const stripeId = 'acct_1SUZqALtgt4FUyPU'; // <-- SEU STRIPE CONNECT ACCOUNT ID
+    const stripeId = 'geral@yogakulabenfica.com'; // <-- SEU STRIPE CONNECT ACCOUNT ID
 
     const result = await pool.query(
       `UPDATE partners SET stripe_account_id = $1 WHERE slug = $2`,
