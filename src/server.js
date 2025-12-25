14241d79f344b968df88191757ecddffa767ebed
Igor Raposo <igorraposo02@gmail.com>
2025-12-25 05:14:06 +0000

    atualização

--- src/server.js
+++ src/server.js
@@ -20,7 +20,8 @@ const allowedOrigins = [
   'https://www.voucherhub.pt',
   'http://localhost:3000',
   'http://localhost:5500',
-  'http://127.0.0.1:5500'
+  'http://127.0.0.1:5500',
+  'null'
 ];
 
 app.use(cors({ origin: allowedOrigins }));
